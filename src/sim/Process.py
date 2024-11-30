b1f2578cef75317eb11e5f258c194ac8b241bfcb
Nicholas Mosier <nmosier@stanford.edu>
2024-11-29 20:36:30 -0800

    sim-se: implement free-list page allocator
    Fix #1824. Implement a free-list-based physical page allocator.
    The previous allocator was naive and never reallocated pages
    that were freed (e.g., by the `munmap` system call). This resulted
    simulated workloads requiring in some cases much larger memories
    than when running natively on the host (in gcc SPEC2017 benchmark,
    up to 4x more).
    
    This commit implements replaces the naive page allocator with
    slightly less naive one that uses a simple free list to track
    ranges of physical pages that are free. The free list is included
    in checkpoints.
    
    Now, whenever a virtual address is unmapped (which, with this patch,
    now only occurs in `MemState::unmapRegion`), its mapped-to physical
    page is marked as deallocated/freed and thus available to be
    reallocated at some later point.
    
    Change-Id: I51c691cf18372ae298f5743feefd6b0ca20a581c

--- src/sim/Process.py
+++ src/sim/Process.py
@@ -52,6 +52,11 @@ class Process(SimObject):
     kvmInSE = Param.Bool("false", "initialize the process for KvmCPU in SE")
     pinInSE = Param.Bool(False, "Intel Pin in SE")
     maxStackSize = Param.MemorySize("64MiB", "maximum size of the stack")
+    zeroPages = Param.Bool(
+        True,
+        "ensure all allocated pages are zero-filled. glibc malloc generally "
+        "requires this. Disable at your own risk.",
+    )
 
     uid = Param.Int(100, "user id")
     euid = Param.Int(100, "effective user id")
