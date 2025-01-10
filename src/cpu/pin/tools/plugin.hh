f5a4eeb8508193c26a6b79637d2bd42b6fad973d
Nicholas Mosier <nmosier@stanford.edu>
2025-01-10 10:04:28 -0800

    add TODOs to plugin.hh

--- src/cpu/pin/tools/plugin.hh
+++ src/cpu/pin/tools/plugin.hh
@@ -10,6 +10,9 @@ struct Plugin
 
     virtual bool enabled() const = 0;
     virtual bool reg() = 0;
+
+    // TODO: Merge cmd into args, so we just have an arg vector (like main functions).
+    // TODO: Consider making the result an ostream, not a string.
     virtual bool command(const std::string &cmd, const std::vector<std::string> &args, std::string &result) { return false; }
 };
 
