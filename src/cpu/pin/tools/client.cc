1914defc39b0a0a416b2b3c9aeaf49f2c185feb4
Nicholas Mosier <nmosier@stanford.edu>
2025-01-10 09:48:22 -0800

    remove obsolete fhist

--- src/cpu/pin/tools/client.cc
+++ src/cpu/pin/tools/client.cc
@@ -12,7 +12,6 @@
 #include "pin.H"
 #include "ops.hh"
 #include "bbv.hh"
-#include "fhist.hh"
 #include "ringbuf.hh"
 #include "debug.hh"
 #include "cpu/pin/regfile.h"
@@ -1054,7 +1053,6 @@ main(int argc, char *argv[])
     // TODO: Use a static function registration list to make it cleaner.
     // FIXME: Migrate all of these to plugins.
     if (!bbv_register() ||
-        !fhist_register() ||
         !pmcount_register() ||
         !qtrace_register() || // TODO: Remove qtrace.
         !instlist_register() ||
--- src/cpu/pin/tools/fhist.hh
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-bool fhist_register();
