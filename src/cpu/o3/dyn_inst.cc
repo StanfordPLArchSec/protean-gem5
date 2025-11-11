9913a4f6502321c01e3abfef788a41040efd63b4
Nicholas Mosier <nmosier@stanford.edu>
2025-11-11 12:01:32 -0800

    PROTEAN: delay-all option + ctrl bugfix + comm width bugfix

--- src/cpu/o3/dyn_inst.cc
+++ src/cpu/o3/dyn_inst.cc
@@ -739,7 +739,7 @@ bool
 DynInst::taintedXmitsTrack() const
 {
     assert(cpu->protean == Protean::Track);
-    if (isUnsquashable())
+    if (cpu->speculationModel == SpeculationModel::AtRet && isUnsquashable())
         assert(yrotXmits <= cpu->untaintBroadcast);
     return yrotXmits > cpu->untaintBroadcast;
 }
@@ -814,6 +814,7 @@ DynInst::setExecuted()
 
     // [Protean-Track] Sanity checks.
     panic_if(cpu->protean != Protean::None &&
+             cpu->speculationModel == SpeculationModel::AtRet &&
              isMemRef() && !isSquashed() && taintedXmits(),
              "setExecuted for tainted transmitter!\n");
 }
